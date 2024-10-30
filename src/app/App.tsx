e41fa31b394c79c354a8e0ab7cbcb67ecec529b5
Susam <susam.buildings@gmail.com>
2024-10-30 12:21:00 +0300

    :art: Optimize styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ConfigProvider } from 'antd';
 import { EditorPage } from '../editor';
+import { theme } from './theme.ts';
 
 import 'antd/dist/reset.css';
 
 export const App: React.FC = () => {
 	return (
 		<React.StrictMode>
-			<ConfigProvider>
+			<ConfigProvider theme={theme}>
 				<EditorPage />
 			</ConfigProvider>
 		</React.StrictMode>
---
+++ src/app/theme.ts
@@ -0,0 +1,9 @@
+import { ThemeConfig } from 'antd';
+
+export const theme: ThemeConfig = {
+	components: {
+		Typography: {
+			titleMarginBottom: 0,
+		},
+	},
+};
